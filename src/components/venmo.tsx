34443c5ea4ef76efa01774c04013a5f3e9f0bd3e
Douglas Miller <doug@douglas-miller.com>
2024-03-15 15:39:51 -0500

    Fixing issues with Venmo

--- src/components/venmo.tsx
+++ src/components/venmo.tsx
@@ -1,37 +1,40 @@
 'use client'
 
 import { useRecurly } from '@recurly/react-recurly';
-import React, { useMemo, useRef } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useConfigurationContext } from './configuration-context';
+import { RecurlyError, TokenPayload } from '@recurly/recurly-js';
 
 
 export function Venmo() {
   const recurly = useRecurly();
-  const formRef = useRef(null);
+  const formRef = useRef(document.createElement('form'));
+  const [form, setForm] = useState(formRef.current);
+  const setFormRef = (ref: HTMLFormElement) => setForm(ref);
 
   const { braintreeAuth } = useConfigurationContext();
 
-  const venmo = useMemo(() => {
-    return recurly.Venmo({
+  const venmo = useMemo(() => recurly.Venmo({
       braintree: { clientAuthorization: braintreeAuth },
-      form: formRef.current
-    });
-
-  }, [recurly, formRef, braintreeAuth]);
+      form: form,
+    }), [recurly, form, braintreeAuth]);
 
   const handleSubmit = (event) => {
     if (event.preventDefault) event.preventDefault();
     venmo.start();
   };
 
-  venmo.on('error', (err) => console.log('ERROR', err.message, err));
-  venmo.on('token', (token) => console.log('TOKEN', token));
+  useEffect(() => {
+    venmo.on('error', (err: RecurlyError) => console.log('ERROR', err.message, err));
+    venmo.on('token', (token: TokenPayload) => console.log('TOKEN', token));
+  }, [venmo])
+
 
   return (
-    <form onSubmit={handleSubmit} ref={formRef}>
+    <form onSubmit={handleSubmit} ref={setFormRef}>
       <input className="text-slate-600" type="text" data-recurly="first_name" placeholder="First name" />
       <input className="text-slate-600" type="text" data-recurly="last_name" placeholder="Last name" />
-      <button>Pay with Venmo</button>
+      { venmo && <button>Pay with Venmo</button> }
     </form>
   );
 }
